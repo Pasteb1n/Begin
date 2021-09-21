ab7b3a410514903b09ded62db5b85929e2c91c29
Pasteb1n <88246245+Pasteb1n@users.noreply.github.com>
2021-09-21 22:21:04 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,33 +1,35 @@
-while True:
-    ex = input("Press y to continue: ")
-    if ex.lower() == "y":
-        print('Welcome')
-    else:
-        break
-    try:
-        onenumber = int(input("Enter the first number: "))
-    except ValueError:
-        print("You must enter a number!")
-        continue
-    try:
-        twonumber = int(input("Enter the second number: "))
-    except ValueError:
-        print("You must enter a number!")
-        continue
-    znak = input("+; -; *; /; Select arithmetic operation: ")
-    if znak == "+":
-        print(onenumber + twonumber)
-    elif znak == "-":
-        print(onenumber - twonumber)
-    elif znak == "/":
+def main():
+    while True:    
+        ex = input("Press y to continue: ")
+        if ex.lower() == "y":
+            print('Welcome')
+        else:
+            break
         try:
-            print(onenumber / twonumber)
-        except ZeroDivisionError:
-            print("Cannot be divided by zero")
+            onenumber = int(input("Enter the first number: "))
+        except ValueError:
+            print("You must enter a number!")
             continue
-    elif znak == "*":
-        print(onenumber * twonumber)
-    elif znak == "**":
-        print(onenumber ** twonumber)
-    else:
-        print("Unknown arithmetic operation")
+        try:
+            twonumber = int(input("Enter the second number: "))
+        except ValueError:
+            print("You must enter a number!")
+            continue
+        znak = input("+; -; *; /; Select arithmetic operation: ")
+        if znak == "+":
+            print(onenumber + twonumber)
+        elif znak == "-":
+            print(onenumber - twonumber)
+        elif znak == "/":
+            try:
+                print(onenumber / twonumber)
+            except ZeroDivisionError:
+                print("Cannot be divided by zero")
+                continue
+        elif znak == "*":
+            print(onenumber * twonumber)
+        elif znak == "**":
+            print(onenumber ** twonumber)
+        else:
+            print("Unknown arithmetic operation")
+main()
